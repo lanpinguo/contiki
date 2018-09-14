dd973fe2fd57c53d844759e7bc2daea857c741c1
lanpinguo <lanpinguo1985@126.com>
2018-09-14 16:34:49 +0800

    add PureX project

---
+++ motes/PureX/project-conf.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+  Copyright (C), 2001-2011, Pure Co., Ltd.
+
+ ******************************************************************************
+  File Name     : pureX.c
+  Version       : Initial Draft
+  Author        : lanpinguo
+  Created       : 2018/9/14
+  Last Modified :
+  Description   :  Project config file
+  Function List :
+              broadcast_recv
+              PROCESS_THREAD
+              PROCESS_THREAD
+              PROCESS_THREAD
+              recv_uc
+  History       :
+  1.Date        : 2018/9/14
+    Author      : lanpinguo
+    Modification: Created file
+
+******************************************************************************/
+
+
+#ifndef __PROJECT_CONF_H__
+#define __PROJECT_CONF_H__
+
+#define XMEM_ERASE_UNIT_SIZE 1024 /*1K*/
+
+
+
+#endif /* __PROJECT_CONF_H__ */
